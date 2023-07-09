53b3475403ca333a1e48cbb8fae3eb4ae25fbe83
Malú <130713333+malummartiins@users.noreply.github.com>
2023-07-09 18:39:20 -0300

    Code do desafio
    primeiro código do desafio

---
+++ desafio.py
@@ -0,0 +1,69 @@
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques  = 0
+LIMITES_SAQUES = 3
+
+while True:
+
+    opcao = input (menu)
+
+    if opcao == "d":
+        valor = float (input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor 
+            print ("--  Depósito realizado com sucesso -- ")
+            extrato += f"Depósito: R$ {valor:.2f} \n"
+            
+        else:
+            print ("Operação falhou! O valor informado é inválido.")
+    
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITES_SAQUES
+
+        if excedeu_saldo:
+            print ("Operação falhou! Saldo insuficiente.")
+
+        elif excedeu_limite:
+            print ("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print ("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor 
+            print (" -- Saque realizado com sucesso! -- ")
+            extrato += f"Saque: R$ {valor:.2f}\n "
+            numero_saques += 1
+
+        else:
+            print ("Operação falhou! O valor informado é inválido.")
+
+        #print ("Saque")
+
+    elif opcao == "e":
+        print("\n ============= EXTRATO =============")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print (f"\nSaldo: R$ {saldo:.2f}")
+        print("======================================")
+        #print ("Extrato")
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, por favor, selecione novamente a operação desejada.")
